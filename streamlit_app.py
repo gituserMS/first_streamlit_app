66cb430c34defea17987c2a7605d5c5edd556c16
gituserMS <141670702+gituserMS@users.noreply.github.com>
2023-08-07 23:57:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
