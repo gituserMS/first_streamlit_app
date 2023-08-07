74209b3e868241397f3658de255e7a4ed0efd3c5
gituserMS <141670702+gituserMS@users.noreply.github.com>
2023-08-07 23:55:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title('Welcome to the food store')
-streamlit.header('Breakfast  Menu')
-streamlit.text('  veg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
